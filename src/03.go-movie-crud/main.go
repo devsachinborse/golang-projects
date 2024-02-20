b7d3e1af112c9da7f3e54e67eebb6380a578e1fe
Sachin Borse <111965224+SachinBorse009@users.noreply.github.com>
2024-02-20 15:08:22 +0530

    Update main.go

--- src/03.go-movie-crud/main.go
+++ src/03.go-movie-crud/main.go
@@ -73,7 +73,7 @@ func deleteMovie(w http.ResponseWriter, r *http.Request){
 func getMovie(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "application/json")
 	params:=mux.Vars(r)
-	for _, item := range movies   {                               //use _ blank itentifier because we dont want to use index here
+	for _, item := range movies   {                               //use _ blank identifier because we dont want to use index here
 		if item.ID == params["id"]{
 			json.NewEncoder(w).Encode(item)
 		}
